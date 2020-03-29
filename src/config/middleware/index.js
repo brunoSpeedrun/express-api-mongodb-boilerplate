4942a743f806e5eb86290809d3b8bc71ebcc51a8
Bruno Xavier <bruno.xavier.moura@gmail.com>
2020-03-28 23:47:40 -0300

    enable cors.

--- src/config/middleware/index.js
+++ src/config/middleware/index.js
@@ -3,9 +3,17 @@ const helmet = require('helmet')
 const compression = require('compression')
 const expressStatusMonitor = require('express-status-monitor')
 const morgan = require('morgan')
+const cors = require('cors')
 
 module.exports = {
   apply(app) {
+    app.use(cors({
+      allowedHeaders: '*',
+      credentials: true,
+      exposedHeaders: '*',
+      methods: ['GET, POST, PUT, PATCH, DELETE, OPTIONS'],
+      origin: '*'
+    }))
     app.use(helmet())
     app.use(expressStatusMonitor())
     app.use(compression())
