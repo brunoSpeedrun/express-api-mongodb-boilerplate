d8af1123737e2ec7d492dac92216f150ff1e41ca
Bruno Xavier <bruno.xavier.moura@gmail.com>
2020-03-28 21:04:02 -0300

    adding code style config.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+
 const app = express()
 
 app.use('/', (req, res) => {
-    res.json({
-        code: 200,
-        message: 'It\'s works!'
-    })
+  res.json({
+    code: 200,
+    message: "It's works!",
+  })
 })
 
 module.exports = app
